7cd0ca03a8b4ee96a26468bb71dbd355db2e4a8a
Dhruv Sharma <dhruv004@e.ntu.edu.sg>
2019-04-02 14:40:36 +0800

    Initial Commit

---
+++ ElementQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ElementQuiz
+//
+//  Created by Dhruv Sharma on 2/4/19.
+//  Copyright © 2019 Dhruv Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
